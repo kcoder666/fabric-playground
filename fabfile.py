f757f25633cbc6377f62b2ce20a4170f47c4167b
Khoa Tran <khoa.tran@notabasement.com>
2016-06-20 16:47:49 +0700

    Handling test failure

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,23 @@
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+def test():
+    with settings(warn_only=True):
+        result = local("./manage.py test web_app", capture=True)
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
+
+
+def commit():
+    local("git add -i && git commit")
+
+
+def push():
+    local("git push")
+
 
 def prepare_deploy():
-    local("./manage.py test web_app")
-    local("git add -i && git commit")
-    local("git push")
+    test()
+    commit()
+    push()
 
